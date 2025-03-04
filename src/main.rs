4e55d43245ade9290cd6aef0cdeff42669bae4ba
Vincent <v_mulder@hotmail.com>
2025-03-04 17:27:16 +0100

    feat: tests for all endpoints

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Should bind to socket address with random port");
+    println!("listening on {:?}", listener.local_addr());
     run(listener)?.await
 }
